8b5d662914ef106e22c4fcce7f50574946b71bf8
YungMilky <63095356+YungMilky@users.noreply.github.com>
2020-12-30 14:18:04 +0100

    Buncha stuff

--- Dinder/Controllers/UserController.cs
+++ Dinder/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using DinderDL;
+﻿using Dinder.Data;
+using Dinder.Models;
+using DinderDL;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,17 +12,36 @@ namespace Dinder.Controllers
     public class UserController : Controller
     {
         private readonly UserEntityContext _uecontext;
+        private ApplicationDbContext _appDbContext;
 
         public UserController(UserEntityContext uecontext)
         {
             _uecontext = uecontext;
+
+            var user = _uecontext.Users.SingleOrDefault(u => u.Email == User.Identity.Name);
+            if (user == null)
+            {
+                uecontext.Users.Add(new DinderDL.Models.UserEntity
+                {
+                    Email = User.Identity.Name
+                });
+                _uecontext.SaveChanges();
+            }
         }
 
         public IActionResult Index()
         {
-            //var users = _uecontext.Users.;
 
             return View();
+        }
+
+        public IActionResult ShowUsers()
+        {
+            var userEntities = _uecontext.Users.ToList();
+            //lala
+            return View(new UserViewModel { 
+            
+            });
         }
     }
 }
