3c8e0b93f8146ce8e1b54121158848c51851315c
YungMilky <63095356+YungMilky@users.noreply.github.com>
2021-01-06 16:17:03 +0100

    userentities API

--- Dinder/Controllers/UserController.cs
+++ Dinder/Controllers/UserController.cs
@@ -11,36 +11,17 @@ namespace Dinder.Controllers
 {
     public class UserController : Controller
     {
-        private readonly UserEntityContext _uecontext;
         private ApplicationDbContext _appDbContext;
 
-        public UserController(UserEntityContext uecontext)
+        public UserController()
         {
-            _uecontext = uecontext;
-            var user = _uecontext.Users.SingleOrDefault(u => u.Email == User.Identity.Name);
-            if (user == null)
-            {
-                uecontext.Users.Add(new DinderDL.Models.UserEntity
-                {
-                    Email = User.Identity.Name
-                });
-                _uecontext.SaveChanges();
-            }
+          
         }
 
         public IActionResult Index()
         {
 
             return View();
-        }
-
-        public IActionResult ShowUsers()
-        {
-            var userEntities = _uecontext.Users.ToList();
-            //lala
-            return View(new UserViewModel { 
-            
-            });
         }
     }
 }
---
+++ Dinder/Controllers/UserEntitiesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DinderDL;
+using DinderDL.Models;
+
+namespace Dinder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserEntitiesController : ControllerBase
+    {
+        private readonly UserEntityContext _context;
+
+        public UserEntitiesController(UserEntityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserEntities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserEntity>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/UserEntities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserEntity>> GetUserEntity(int id)
+        {
+            var userEntity = await _context.Users.FindAsync(id);
+
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+
+            return userEntity;
+        }
+
+        // PUT: api/UserEntities/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUserEntity(int id, UserEntity userEntity)
+        {
+            if (id != userEntity.UserID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(userEntity).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserEntityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/UserEntities
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<UserEntity>> PostUserEntity(UserEntity userEntity)
+        {
+            _context.Users.Add(userEntity);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUserEntity", new { id = userEntity.UserID }, userEntity);
+        }
+
+        // DELETE: api/UserEntities/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUserEntity(int id)
+        {
+            var userEntity = await _context.Users.FindAsync(id);
+            if (userEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(userEntity);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserEntityExists(int id)
+        {
+            return _context.Users.Any(e => e.UserID == id);
+        }
+    }
+}
