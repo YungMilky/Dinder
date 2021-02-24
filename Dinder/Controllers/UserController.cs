02ac8032c44d9697d01bba8fe7b1952559256cd4
YungMilky <63095356+YungMilky@users.noreply.github.com>
2021-02-24 21:40:20 +0100

    Posts working

--- Dinder/Controllers/UserController.cs
+++ Dinder/Controllers/UserController.cs
@@ -35,7 +35,17 @@ namespace Dinder.Controllers
         [Route("getProfile/{userid}")]
         public IActionResult Profile(int userid)
         {
-            var userModel = _uecontext.Users.Where(u => u.UserID == userid).Include(u => u.ReceivedPosts).ToList();
+            //get profile of user with ID userid
+            var userModel = _uecontext.Users.Where(u => u.UserID == userid).Include(u=>u.ReceivedPosts).ToList();
+
+            //get poster IDs of this profile
+            var posterID = _uecontext.Posts.Where(p => p.ReceiverID == userid).Select(p => p.PosterID);
+
+            //match poster IDs with any user IDs in the database, to get names
+            var posterNames = _uecontext.Users.Where(u => posterID.Contains(u.UserID))
+                .ToDictionary(u =>u.UserID, u=>u.Name);
+
+                //.Select(u => new KeyValuePair<int, string>(u.PosterID, u.Name)).ToList();
 
             var friendIDs = _uecontext.Friendships.Where(f => f.FriendStatus == true && (f.Friend1ID == userid || f.Friend2ID == userid))
                                                     .ToList();
@@ -44,14 +54,13 @@ namespace Dinder.Controllers
                                             .Select(f => f.Friend1ID).Contains(u.UserID) || friendIDs.Select(f => f.Friend2ID).Contains(u.UserID))
                                             .ToList();
 
-            var profileModel = new ProfileViewModel
+            return View(new ProfileViewModel
             {
                 CurrentUserID = _uecontext.Users.First(id => id.Email == User.Identity.Name).UserID,
                 User = userModel,
+                Posters = posterNames,
                 Friends = friends
-            };
-
-            return View(profileModel);
+            });
         }
 
         //public IActionResult Friends(int userid)
