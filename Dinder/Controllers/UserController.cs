423d10c0d715a4d3b5e402ca8d759986752edbdc
YungMilky <63095356+YungMilky@users.noreply.github.com>
2021-02-24 21:56:26 +0100

    freshly submitted posts have working links, but name is taken from current profile instead of logged in profile

--- Dinder/Controllers/UserController.cs
+++ Dinder/Controllers/UserController.cs
@@ -41,11 +41,9 @@ namespace Dinder.Controllers
             //get poster IDs of this profile
             var posterID = _uecontext.Posts.Where(p => p.ReceiverID == userid).Select(p => p.PosterID);
 
-            //match poster IDs with any user IDs in the database, to get names
+            //get poster IDs with any user IDs in the database, along with poster names, to later display names by poster IDs in the view
             var posterNames = _uecontext.Users.Where(u => posterID.Contains(u.UserID))
                 .ToDictionary(u =>u.UserID, u=>u.Name);
-
-                //.Select(u => new KeyValuePair<int, string>(u.PosterID, u.Name)).ToList();
 
             var friendIDs = _uecontext.Friendships.Where(f => f.FriendStatus == true && (f.Friend1ID == userid || f.Friend2ID == userid))
                                                     .ToList();
